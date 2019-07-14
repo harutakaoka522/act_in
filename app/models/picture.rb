3d55a4e515b57b39bbd587ef658ef1c6eddcfa92
hogehoge <AAAgmail.com>
2019-07-14 17:41:03 +0900

    picture画面のmodal実装

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,7 @@ class Picture < ApplicationRecord
     has_many :picture_labels, dependent: :destroy
     #, inverse_of: :picture
     mount_uploader :image, ImageUploader
+    validates :image_title, presence: true, length: { maximum: 30 }
+    validates :image, presence: true
+    validates :image_content, presence: true, length: { maximum: 255 }
 end
