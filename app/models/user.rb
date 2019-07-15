f27dab11cea029f05ea8600cbcda8ef476e59da2
hogehoge <AAAgmail.com>
2019-07-15 16:16:05 +0900

    labelの削除権限の設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :pictures, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :labels, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_045348) do
+ActiveRecord::Schema.define(version: 2019_07_15_070601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_07_15_045348) do
     t.string "label_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_labels_on_user_id"
   end
 
   create_table "picture_labels", force: :cascade do |t|
@@ -69,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_045348) do
 
   add_foreign_key "comments", "pictures"
   add_foreign_key "comments", "users"
+  add_foreign_key "labels", "users"
   add_foreign_key "picture_labels", "labels"
   add_foreign_key "picture_labels", "pictures"
   add_foreign_key "pictures", "users"
